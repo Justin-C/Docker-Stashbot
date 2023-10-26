d2304472321d9fbb9e398375467e032e7fa59e14
Justin C <justin.cai101@gmail.com>
2023-10-25 23:40:12 -0700

    feat: working combo

--- ui/src/components/find-item-field.tsx
+++ ui/src/components/find-item-field.tsx
@@ -1,64 +1,98 @@
-import { Autocomplete, Button, TextInput } from "@mantine/core";
+import { Autocomplete, Button, Combobox, Loader, TextInput, useCombobox } from "@mantine/core";
 import { useEffect, useState } from "react";
 import { useDebouncedCallback } from "use-debounce";
 import { fetchFindItems } from "./helpers/api/api";
+import { Item } from "./helpers/api/types";
 
 export const FindItemField = () => {
-  // const form = useForm();
-  const [autocompleteValue, setAutocompleteValue] = useState("");
-  const [autocompleteOptions, setAutocompleteOptions] = useState([""]);
-  const [foundItems, setFoundItems] = useState([
-    { itemName: "", itemLocation: "" },
-  ]);
+  const [foundItems, setFoundItems] = useState<Item[]>([]);
+  const [comboOptions, setComboOptions] = useState<React.ReactNode[]>([])
+  const [loading, setLoading] = useState(false);
+  const [value, setValue] = useState('');
+  
   const [selectedItem, setSelectedItem] = useState({
     itemName: "",
     itemLocation: "",
   });
   const debouncedFetch = useDebouncedCallback(fetchAutocompleteData, 500);
 
-  useEffect(() => {
-    if(autocompleteValue.length > 1){
-      debouncedFetch(autocompleteValue);
-    }
-  }, [autocompleteValue, debouncedFetch]);
 
-  useEffect(() => {
-    const search = foundItems.find(
-      (item) => item?.itemName === autocompleteValue,
-    );
-    if (search) {
-      setSelectedItem(search);
-    }
-  }, [foundItems, autocompleteValue]);
-
-  // const handleSubmit = (values: any) => {
-  //   // Handle form submission
-  //   console.log(values);
-  // };
 
   function fetchAutocompleteData(query: any) {
+    setLoading(true);
+
     const fetchItems = async () => {
       const response = await fetchFindItems(query);
       if (response && response.foundItems) {
-        const itemNames = response.foundItems.map((item: any) => item.itemName);
         setFoundItems(response.foundItems);
-        setAutocompleteOptions(itemNames);
+        setLoading(false);
+
       }
     };
 
     fetchItems();
   }
 
+
+  const combobox = useCombobox({
+    onDropdownClose: () => combobox.resetSelectedOption(),
+  });
+
+  
+
+  useEffect(() => {
+    if(value.length > 1){
+      debouncedFetch(value);
+    }
+  }, [value, debouncedFetch]);
+
+  useEffect(() => {
+    const options = (foundItems || []).map((item) => (
+      <Combobox.Option value={item.itemName} key={item.itemName}>
+        {item.itemName}
+      </Combobox.Option>
+    ));
+    console.log(options)
+    setComboOptions(options)
+  }, [foundItems])
+
+  useEffect(() => {
+    // we need to wait for options to render before we can select first one
+    combobox.selectFirstOption();
+  }, [value, comboOptions]);
+
   return (
     <div>
-      <Autocomplete
-        label="Find an Item"
-        placeholder="Find an Item"
-        // data={autocompleteOptions}
-        value={autocompleteValue}
-        onChange={(value) => setAutocompleteValue(value)}
-        required
-      />
+    <Combobox
+      onOptionSubmit={(optionValue) => {
+        setValue(optionValue);
+        combobox.closeDropdown();
+      }}
+      store={combobox}
+      withinPortal={false}
+    >
+      <Combobox.Target>
+        <TextInput
+          label="Pick value or type anything"
+          placeholder="Pick value or type anything"
+          value={value}
+          onChange={(event) => {
+            setValue(event.currentTarget.value);
+            combobox.openDropdown();
+          }}
+          onClick={() => combobox.openDropdown()}
+          onFocus={() => combobox.openDropdown()}
+          onBlur={() => combobox.closeDropdown()}
+          rightSection={loading && <Loader size={18} />}
+        />
+      </Combobox.Target>
+
+      <Combobox.Dropdown>
+        <Combobox.Options>
+          {comboOptions.length === 0 ? <Combobox.Empty>Nothing found</Combobox.Empty> : comboOptions}
+        </Combobox.Options>
+      </Combobox.Dropdown>
+    </Combobox>
       <label htmlFor="">
         {selectedItem.itemName} in bin {selectedItem.itemLocation}
       </label>
